13ae845b2101d761bc73e289da1fde89fb231cbf
unknown <rakkakeren@gmail.com>
2022-10-25 12:57:54 +0700

    change: more data types

--- lesson04_data_types.php
+++ lesson04_data_types.php
@@ -2,4 +2,21 @@
     $name = "Rakka Alhazimi"; // string
     $age = 20; // integer
     $gpa = 3.56; // float
+    $isGood = true; // boolean
+    $awards = array("best student", "high ranker"); // array
+    $girlfriends = null; // null
+
+    // Object
+    class Student {
+        public $name;
+        public $age;
+        public $gpa;
+        public function __construct($name, $age, $gpa) {
+            $this -> name = $name;
+            $this -> age = $age;
+            $this -> gpa = $gpa;
+        }
+    }
+
+    $myStudent = new Student($name, $age, $gpa);
 ?>
